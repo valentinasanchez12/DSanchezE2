ce7803b11fd8cf7163e557f3e551a6d287e6ea8e
valentinasanchez12 <valen12345lara@gmail.com>
2023-01-12 20:24:52 -0500

    proyecto

---
+++ citas-react/src/App.jsx
@@ -0,0 +1,26 @@
+/*funcion creadora de la aplicacion */
+function App() {
+ 
+
+ return (
+    /* etiqueta padre debajo queda el body de la pagina*/
+   <>
+        
+    <div className="App">
+        
+          <h1>Ejemplo</h1>
+        
+    </div>
+     <div className="App">
+          
+          <h1>Ejemplo2</h1>
+        
+    </div>
+           
+    </>
+
+ 
+  )
+}
+/*Exportar para importar el moduloa otras partes de la app*/
+export default App
